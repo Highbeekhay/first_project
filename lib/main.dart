68e2d324d06bae1eb378e2abd69023d23410c0f2
Highbeekhay <118852889+Highbeekhay@users.noreply.github.com>
2023-01-18 16:34:25 +0100

    first step

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,46 @@
-void main() {}
+import 'package:flutter/material.dart';
 
-class MyApp {}
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What is your favorite color?',
+      'What is your favorite animal?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First Project'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 1'),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 2'),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
